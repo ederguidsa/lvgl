353b851050e913386fecf025c61007523f8f0a68
eder <ederguidsa@gmail.com>
2019-01-08 09:48:22 -0800

    lv_roller: estava ignorando key up/down

--- lv_objx/lv_roller.c
+++ lv_objx/lv_roller.c
@@ -388,11 +388,11 @@ static lv_res_t lv_roller_signal(lv_obj_t * roller, lv_signal_t sign, void * par
 
     } else if(sign == LV_SIGNAL_CONTROLL) {
         char c = *((char *)param);
-        if(c == LV_GROUP_KEY_RIGHT /*|| c == LV_GROUP_KEY_DOWN*/) {
+        if(c == LV_GROUP_KEY_RIGHT || c == LV_GROUP_KEY_DOWN) {
 //            if(ext->ddlist.sel_opt_id + 1 < ext->ddlist.option_cnt) {
                 lv_roller_set_selected(roller, ext->ddlist.sel_opt_id + 1, true);
 //            }
-        } else if(c == LV_GROUP_KEY_LEFT /*|| c == LV_GROUP_KEY_UP*/) {
+        } else if(c == LV_GROUP_KEY_LEFT || c == LV_GROUP_KEY_UP) {
 //            if(ext->ddlist.sel_opt_id > 0) {
                 lv_roller_set_selected(roller, ext->ddlist.sel_opt_id - 1, true);
 //            }
